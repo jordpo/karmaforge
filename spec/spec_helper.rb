4a28c678490ee583066ef8ed387a400330589440
K Mak <karen.mak@stx.oxon.org>
2014-03-01 11:32:02 -0500

    created directories for factories and features, added format documentation to .rspec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,15 +3,15 @@ ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 require 'rspec/autorun'
+# require 'capybara/rails'
+# require 'capybara/rspec'
 require 'simplecov'
 require 'capybara/poltergeist'
 
 SimpleCov.start
 Capybara.javascript_driver = :poltergeist
 
-
 Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
-
 
 ActiveRecord::Migration.check_pending! if defined?(ActiveRecord::Migration)
 
@@ -20,6 +20,7 @@ FactoryGirl.lint
 RSpec.configure do |config|
  config.include FactoryGirl::Syntax::Methods
  config.include SessionHelpers
+ config.use_transactional_fixtures = false
  config.order = 'random'
  config.treat_symbols_as_metadata_keys_with_true_values = true
 end
