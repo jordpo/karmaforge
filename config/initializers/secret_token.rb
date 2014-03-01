1a248844afb7e09c281e3bf48e0cc0dca1ba302c
Jordan Morano <jordan.morano@gmail.com>
2014-03-01 10:39:49 -0500

    Initialize Rails App

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+KarmaForge::Application.config.secret_key_base = '4d120b50f52e6bce00ad52b4db7982840d13fc09c7904e8c5b77e2e714de186d61580a249fd1ea18b13e164f8fe5b01bee94c17bbb691501c2a97f8e706a840d'
