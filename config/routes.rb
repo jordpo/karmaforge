93ef36b0117aa5edb7c65100576cc6d2b279c16e
Jordan Morano <jordan.morano@gmail.com>
2014-03-03 11:20:31 -0500

    Add devise with User and Omniauth FB; update views with NAV elements

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 KarmaForge::Application.routes.draw do
+  devise_for :users, controllers: {omniauth_callbacks: 'omniauth_callbacks'}
   mount JasmineRails::Engine => '/specs' if defined?(JasmineRails)
   root to: 'dashboard#index'
 end
