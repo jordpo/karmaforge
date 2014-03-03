126f6ddbd1c16c400e7fef256a582338018ed582
Becky Bell <rlbell@alum.bu.edu>
2014-03-03 16:29:15 -0500

    created location.js file in javascript assets folder

--- spec/models/location_spec.rb
+++ spec/models/location_spec.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: locations
+#
+#  id         :integer          not null, primary key
+#  city       :string(255)
+#  state      :string(255)
+#  created_at :datetime
+#  updated_at :datetime
+#
+
 require 'spec_helper'
 
 describe Location do
