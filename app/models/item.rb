7050bdfd7abb4b2c47db1f6c8896e86a88afb55b
K Mak <karen.mak@stx.oxon.org>
2014-03-03 11:45:02 -0500

    added testing for validation and association of the Item and Location models

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,4 +2,8 @@ class Item < ActiveRecord::Base
   has_many :transactions
   has_many :locations, through: :transactions
   has_many :users, through: :transactions
+
+  validates :name, presence: true
+  validates :average_price, presence: true
+  validates :average_bid, presence: true
 end
--- app/models/location.rb
+++ app/models/location.rb
@@ -2,4 +2,7 @@ class Location < ActiveRecord::Base
   has_many :transactions
   has_many :items, through: :transactions
   has_many :users, through: :transactions
+
+  validates :city, presence: true
+  validates :state, presence: true
 end
