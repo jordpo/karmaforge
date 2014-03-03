173887178e26d3d54d43ec2aef653a1231693246
Jordan Morano <jordan.morano@gmail.com>
2014-03-03 12:39:25 -0500

    Update spec_helper so simplecov loads before all models

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,10 @@
+require 'simplecov'
+require 'capybara/poltergeist'
+require 'capybara/rspec'
+
+SimpleCov.start 'rails'
+Capybara.javascript_driver = :poltergeist
+
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
@@ -5,12 +12,6 @@ require 'rspec/rails'
 require 'rspec/autorun'
 # require 'capybara/rails'
 # require 'capybara/rspec'
-require 'simplecov'
-require 'capybara/poltergeist'
-require 'capybara/rspec'
-
-SimpleCov.start 'rails'
-Capybara.javascript_driver = :poltergeist
 
 Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
 
