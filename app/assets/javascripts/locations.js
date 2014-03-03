abab7645028b0249064f49076bbd28b5cb9e90fb
Becky Bell <rlbell@alum.bu.edu>
2014-03-03 18:39:40 -0500

    moved locations.js file to new branch

---
+++ app/assets/javascripts/locations.js
@@ -0,0 +1,25 @@
+var KarmaForge = KarmaForge || {};
+
+
+    KarmaForge.Location = function(city, state) {
+      this.city = city;
+      this.state = state;
+    }
+
+    KarmaForge.Location.prototype.save = function(event) {
+
+    }
+
+    var $form =$(event.target),
+    $city = $form.find("input[name='city']"),
+    $state = $form.find("input[name='state']"),
+    KarmaForge.action = $form.attr('action');
+
+    event.preventDefault();
+
+    $.ajax({
+      type: "POST",
+      url: "/"
+      data: {location}
+
+    })
