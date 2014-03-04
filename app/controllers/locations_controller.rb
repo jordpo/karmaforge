3667fd1821642bfef20c47e6c65e56ebecc73d3e
Jordan Morano <jordan.morano@gmail.com>
2014-03-04 18:07:18 -0500

    Add session for Location to prepopulate with values used from last location

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -3,10 +3,13 @@ class LocationsController < ApplicationController
   def create
     @location = Location.where(location_params).first
     if @location
+      session[:current_location] = @location.id
       render json: @location
     else
       @location = Location.new(location_params)
-      @location.save
+      if @location.save
+        session[:current_location] = @location.id
+      end
       render json: @location
     end
   end
