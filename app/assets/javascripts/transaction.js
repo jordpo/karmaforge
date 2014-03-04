8931dd77918d6d691b0ef9e0a686d68788576704
K Mak <karen.mak@stx.oxon.org>
2014-03-04 11:51:03 -0500

    fixed syntax error for transactions.js

--- app/assets/javascripts/transaction.js
+++ app/assets/javascripts/transaction.js
@@ -4,12 +4,12 @@ KarmaForge.Transaction = function(user_id) {
   this.user_id = KarmaForge.currentUser.id;
 }
 
-KarmaForge.Transaction.prototype.render = function(KarmaForge.currentItem, KarmaForge.currentLocation) {
-  var this.item_id = KarmaForge.currentItem.id,
-      this.location_id = KarmaForge.currentLocation.id,
-      this.karma_point = KarmaForge.currentItem.bid * KarmaForge.currentItem.price * Math.random(),
-      transactionDiv = document.getElementById('transaction-display');
+KarmaForge.Transaction.prototype.render = function(currentItem, currentLocation) {
+  this.item_id = KarmaForge.currentItem.id,
+  this.location_id = KarmaForge.currentLocation.id,
+  this.karma_point = KarmaForge.currentItem.bid * KarmaForge.currentItem.price * Math.random(),
+  transactionDiv = document.getElementById('transaction-display');
 
-  transactionDiv.innerhtml = "<p>You have forged" + this.karma_point + " karma points for " + this.location.city + ", " + this.location.state " !</p>";
+  transactionDiv.append(this.karma_point, this.location.city, this.location.state);
 }
 
