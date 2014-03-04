a40f74b38ca5ef10c2c3da490e4b4b06c9592239
Becky Bell <rlbell@alum.bu.edu>
2014-03-04 17:15:53 -0500

    changed location city and state to .toUpperCase

--- app/assets/javascripts/karmaforge.js
+++ app/assets/javascripts/karmaforge.js
@@ -10,7 +10,7 @@ KarmaForge.saveLocation = function (event) {
     location;
   event.preventDefault();
 
-  this.currentLocation = new this.Location($city.val().toTitleCase, $state.val().toTitleCase);
+  this.currentLocation = new this.Location($city.val().toUpperCase(), $state.val().toUpperCase());
   location = this.currentLocation;
 
   if($city.val() === "" || $state.val() === "") {
@@ -119,21 +119,4 @@ KarmaForge.ajaxSetup = function () {
   });
 };
 
-String.prototype.toTitleCase = function(){
-  var smallWords = /^(a|an|and|as|at|but|by|en|for|if|in|nor|of|on|or|per|the|to|vs?\.?|via)$/i;
 
-  return this.replace(/[A-Za-z0-9\u00C0-\u00FF]+[^\s-]*/g, function(match, index, title){
-    if (index > 0 && index + match.length !== title.length &&
-      match.search(smallWords) > -1 && title.charAt(index - 2) !== ":" &&
-      (title.charAt(index + match.length) !== '-' || title.charAt(index - 1) === '-') &&
-      title.charAt(index - 1).search(/[^\s-]/) < 0) {
-      return match.toLowerCase();
-    }
-
-    if (match.substr(1).search(/[A-Z]|\../) > -1) {
-      return match;
-    }
-
-    return match.charAt(0).toUpperCase() + match.substr(1);
-  });
-};
