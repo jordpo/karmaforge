aa49364102584ec285f1db81372434fcccec5b87
Jordan Morano <jordan.morano@gmail.com>
2014-03-06 12:37:57 -0500

    Visitor can add item and get ebay data

--- spec/features/visitor_forges_karma_spec.rb
+++ spec/features/visitor_forges_karma_spec.rb
@@ -18,5 +18,10 @@ feature 'Forge Karma' do
     click_button 'Next'
     expect(page).to have_content "What do you need to get rid of?"
 
+    fill_in 'Enter item description', with: 'Nintendo 3ds'
+    click_button 'eBay'
+    expect(page).to have_content "Nintendo 3ds"
+    click_button 'Results'
+    expect(page).to have_content "Here's an average price of the last 10 items sold on ebay that match your keywords (and an idea of market interest based on average # of bids)"
   end
 end
